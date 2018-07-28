aa7c925ffbe980ba9b7464f4c000643d25f467b9
CristianoDevNet <cristiano.dev.net@gmail.com>
2018-07-28 13:39:41 -0300

    Aprendendo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Contador from "./Contador";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="App-intro">
-          <p>Iniciando com ReactJS</p>
-          <p>
-            Get started, edit <code>src/App.js</code> and save to
-            reload !!
-          </p>
-        </div>
+        {/* <header className="App-header">
+           <img src={logo} className="App-logo" alt="logo" />
+           <h1 className="App-title">Welcome to React</h1>
+         </header>
+         <div className="App-intro">
+           <p>Iniciando com ReactJS</p>
+           <p>
+             Get started, edit <code>src/App.js</code> and save to
+             reload !!
+           </p>
+         </div> */}
+
+        <Contador />
       </div>
     );
   }
---
+++ src/Contador.js
@@ -0,0 +1,39 @@
+import React, {
+  Component
+} from "react";
+
+class Contador extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      contador: 0,
+    };
+  }
+
+  decrementar = () => {
+    this.setState({
+      contador: this.state.contador - 1,
+    });
+  }
+
+  incrementar = () => {
+    this.setState({
+      contador: this.state.contador + 1,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.contador}</h1>
+        <div>
+          <button onClick={this.decrementar}> - </button>
+          <button onClick={this.incrementar}> + </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contador;
