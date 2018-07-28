5590551a06c43d848258c5730aa0ca58e0244e78
CristianoDevNet <cristiano.dev.net@gmail.com>
2018-07-28 09:56:44 -0300

    Acrescentado parágrafo introdutório

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# contador_react
-Introdução ao ReactJS
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -10,9 +10,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <p>Iniciando com ReactJS</p>
+          <p>
+            Get started, edit <code>src/App.js</code> and save to
+            reload.
+          </p>
+        </div>
       </div>
     );
   }
